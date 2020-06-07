fe523536970620bee5ff309a28025eabc4120ba7
anant-bahuguna <anantbahugunaddn@gmail.com>
2020-06-07 23:16:30 +0530

    Reorganize backend files

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require("express");
 require("./config/mongoose");
-
+const path = require("path");
 const userRouter = require("./routes/user");
 const TodoListRouter = require("./routes/todoList");
 
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT;
 
 app.use(express.json({ extended: false }));
 app.use(userRouter);
 app.use(TodoListRouter);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("frontend/build"));
+
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    );
+}
 
 app.listen(port, () => {
     console.log("Server is running on port " + port);
